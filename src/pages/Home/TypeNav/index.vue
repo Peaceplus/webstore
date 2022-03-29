3c5925a3f653d90422fd41498c4404ba504b70da
iiTzTimmy <573844716@qq.com>
2022-03-29 09:21:29 +0800

    修复亿点小问题

--- src/pages/Home/TypeNav/index.vue
+++ src/pages/Home/TypeNav/index.vue
@@ -1,46 +1,40 @@
 <template>
-    <div class="type-nav">
-        <div class="container">
-            <h2 class="classify">商品分类</h2>
-                <nav class="nav">
-                    <a href="#">手机</a>
-                    <a href="#">数码</a>
-                    <a href="#">食品酒饮</a>
-                    <a href="#">家具厨具</a>
-                    <a href="#">电脑办公</a>
-                    <a href="#">个护清洁</a>
-                    <a href="#">母婴童装</a>
-                    <a href="#">服饰</a>
-                    <a href="#">运动户外</a>
-                    <a href="#">家具家装</a>
-                    <a href="#">医疗保健</a>
-                    <a href="#">美妆护肤</a>
-                    <a href="#">奢侈品</a>
-                </nav>
-        </div>
+  <div class="type-nav">
+    <div class="container">
+      <div class="classify">
+        <h2>商品分类</h2>
+        <nav class="nav">
+          <a href="#">手机</a>
+          <a href="#">数码</a>
+          <a href="#">食品酒饮</a>
+          <a href="#">家具厨具</a>
+          <a href="#">电脑办公</a>
+          <a href="#">个护清洁</a>
+          <a href="#">母婴童装</a>
+          <a href="#">服饰</a>
+          <a href="#">运动户外</a>
+          <a href="#">家具家装</a>
+          <a href="#">医疗保健</a>
+          <a href="#">美妆护肤</a>
+          <a href="#">奢侈品</a>
+        </nav>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import "@/assets/css/home.css"
+import "@/assets/css/home.css";
 export default {
-    name: 'TypeNav',
+  name: "TypeNav",
 
-    data() {
-        return {
+  data() {
+    return {};
+  },
 
-        };
-    },
+  mounted() {},
 
-    mounted() {
-
-    },
-
-    methods: {
-
-    },
+  methods: {},
 };
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
