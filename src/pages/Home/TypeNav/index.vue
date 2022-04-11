9bf21582a7b99ad8affd666fa38c95c5e679b48f
Oliver-oliver-oliver <1399590897@qq.com>
2022-04-11 15:43:21 +0800

    轮播图

--- src/pages/Home/TypeNav/index.vue
+++ src/pages/Home/TypeNav/index.vue
@@ -23,6 +23,16 @@
           </el-collapse-transition>
         </div>
       </div>
+      <div class="carouseldiagram">
+              <a>
+                <img v-for="(item,index) in viewList" :key="index" :src="require('@/assets/'+item)" v-show="timer == index" @mouseenter="clear" @mouseleave="change">
+              </a>
+              <span class="up-page" @click="turnpage('up')" @mouseenter="clear" @mouseleave="change"> &lt; </span>
+                <span class="down-page" @click="turnpage('down')" @mouseenter="clear" @mouseleave="change">&gt;</span>
+                <ul class="slide-index">
+                  <li v-for="(x,y) in viewList" :key="y" :class="timer == y? 'active' :''" @click="page(y)" @mouseenter="clear" @mouseleave="change"></li>
+                </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -33,12 +43,50 @@ export default {
   name: "TypeNav",
 
   data:() =>({
-    show_content: true
+    show_content: true,
+    viewList:[
+      "images/1.png",
+      "images/2.png",
+      "images/3.png",
+    ],
+    timer:0,
+    interrupt:null
   }),
 
-  mounted() {},
+  mounted() {
+    this.change()
+  },
 
-  methods: {},
+  methods: {
+    change(){
+      this.interrupt = setInterval(() => {
+        this.timer++;
+        if(this.timer == this.viewList.length){
+          this.timer = 0;
+        }
+      },2000)
+    },
+    clear(){
+      clearInterval(this.interrupt)
+    },
+    turnpage(str){
+      if(str === 'up'){
+        if(this.timer == 0){
+          this.timer = this.viewList.length;
+        }
+        this.timer--;
+      }
+      else if(str === 'down'){
+         this.timer++;
+        if(this.timer == this.viewList.length){
+          this.timer = 0;
+        }
+      }
+    },
+    page(str){
+      this.timer = str;
+    }
+  }
 };
 </script>
 <style lang="scss" scoped></style>
