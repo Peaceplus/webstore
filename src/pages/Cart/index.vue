607e17ff1855fe7ff9b4183b664b3d87eac688e8
iiTzTimmy <573844716@qq.com>
2022-04-30 18:08:24 +0800

    完成部分购物车界面，并存在大量bug

--- src/pages/Cart/index.vue
+++ src/pages/Cart/index.vue
@@ -3,6 +3,11 @@
     <h4 class="cartTitle">我的购物车</h4>
     <div class="cartContainer">
       <el-table :data="productData" class="productTable">
+        <el-table-column width="30">
+          <template slot-scope="scope">
+            <el-checkbox v-model="scope.row.checked"></el-checkbox>
+          </template>
+        </el-table-column>
         <el-table-column label="商品" width="180">
           <template slot-scope="scope">
             <i class="iconfont icon-shopping"></i>
@@ -17,24 +22,37 @@
           </template>
         </el-table-column>
         <el-table-column label="单价(元)" width="180">
-          <template slot-scope="scope">{{scope.row.price}} </template>
+          <template slot-scope="scope">{{ scope.row.price }} </template>
+        </el-table-column>
+        <el-table-column label="数量" width="180">
+          <button href="javascript:;" class="num-reduce" @click="reduceNum">-</button>
+            <input type="text" class="num-input" v-model="productNum">
+            <button href="javascript:;" class="num-add" @click="addNum">+</button>
+        </el-table-column>
+        <el-table-column label="总计(元)" width="180">
+          <template slot-scope="scope"
+            >{{ scope.row.price * productNum }}
+          </template>
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">
-              添加
+            <el-button size="mini" type="danger" @click="handleAdd(scope.$index)">
+              增加
             </el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)"
-            >
+            <el-button size="mini" type="danger" @click="handleDelete(scope.$index)">
               删除
             </el-button>
           </template>
         </el-table-column>
       </el-table>
-      <div class="productForm"></div>
+      <div class="tableBottom">
+        <el-button type="text" class="iconfont icon-delete" @click="deleteChose"
+          >删除已选中商品</el-button
+        >
+        <el-button type="text" class="iconfont icon-shopping" @click="goShopping"
+          >继续购物</el-button
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -46,33 +64,55 @@ export default {
     return {
       productData: [
         {
+          checked: false,
           product: "src/assets/images/5.png",
           name: "佳能相机",
           info: "佳能公司最早的产品之一",
-          price: "5000.00"
+          price: "5000.00",
+          number: "1",
         },
         {
+          checked: false,
           product: "src/assets/images/6.png",
-          name: "伏特加",
-          info: "来自前苏联的伏特加",
-          price: "2000.00"
+          name: "威士忌",
+          info: "来自前苏联的威士忌",
+          price: "2000.00",
         },
         {
+          checked: false,
           product: "src/assets/images/7.png",
           name: "ROG 笔记本电脑",
           info: "ROG出厂的笔记本电脑",
-          price: "7299.00"
+          price: "7299.00",
         },
       ],
+      productNum:"1",
     };
   },
 
   methods: {
-    handleEdit(index, row) {
-      console.log(index, row);
+    handleAdd(index){
+      this.productData.splice(index,0,1)
     },
-    handleDelete(index, row) {
-      console.log(index, row);
+    handleDelete(index) {
+      this.productData.splice(index, 1);
+    },
+    reduceNum(){
+      if(this.productNum >= 1){
+        this.productNum--;
+        }
+
+    },
+    addNum(){
+      this.productNum++;
+    },
+    deleteChose(index) {
+      this.productData = this.productData.filter(function (index) {
+        return !index.checked;
+      });
+    },
+    goShopping() {
+      this.$router.push("/category");
     },
   },
 };
