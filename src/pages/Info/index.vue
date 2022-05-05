44d1a5c576c1e76b7f568baa05fd6890adf50404
Oliver-oliver-oliver <1399590897@qq.com>
2022-05-05 14:19:21 +0800

    个人中心

--- src/pages/Info/index.vue
+++ src/pages/Info/index.vue
@@ -1,8 +1,11 @@
 @<template>
 <div class="personal">
+  <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="首页" name="1">首页</el-tab-pane>
+  </el-tabs>
   <div class="head">
     <div class="avatar">
-      <img src="@/assets/images/8.png">
+      <el-avatar :shape="circle" :src="ur"></el-avatar>
     </div>
   </div>
 </div>
@@ -10,7 +13,11 @@
 
 <script>
 export default {
-
+  data(){
+    return{
+      ur:"@/assets/images/8.png",
+    }
+  },
 }
 </script>
 
