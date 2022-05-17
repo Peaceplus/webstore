10b174a5052c36f67c030ba985c468764ed853ef
Oliver-oliver-oliver <1399590897@qq.com>
2022-05-17 08:18:56 +0800

    个人中心头像

--- src/pages/Info/index.vue
+++ src/pages/Info/index.vue
@@ -1,13 +1,16 @@
 @<template>
 <div class="personal">
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="首页" name="1">首页</el-tab-pane>
-  </el-tabs>
-  <div class="head">
+  <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick" class="personalContainer">
+    <el-tab-pane label="首页" name="1">
+      <div class="head">
+        <img src="@/assets/images/9.png">
     <div class="avatar">
-      <el-avatar :shape="circle" :src="ur"></el-avatar>
+      <el-avatar :shape="circle" :src="require('@/assets/' + ur)" :fit="fill" :size="100"></el-avatar>
     </div>
   </div>
+    </el-tab-pane>
+    <el-tab-pane label="消费记录" name="2"></el-tab-pane>
+  </el-tabs>
 </div>
 </template>
 
@@ -15,9 +18,15 @@
 export default {
   data(){
     return{
-      ur:"@/assets/images/8.png",
+      activeName:"1",
+      ur:'images/8.png',
     }
   },
+  methods:{
+    handleClick(tab,event){
+      console.log(tab,event);
+    }
+  }
 }
 </script>
 
